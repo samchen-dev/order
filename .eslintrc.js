8d5846d7e4863de7f28eba599f15d0e14f269bab
samchen-dev <samchen@sinabuddy.com>
2020-05-14 18:26:40 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,19 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    "space-before-function-paren": 0,
+    "no-tabs": "off",
+    "indent": ["off", 2],
+    'no-unused-vars': 'off',
+    "semi": ['off', "always"],
+    // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    "comma-dangle": [2, "never"],
+
+    // allow async-await
+    'generator-star-spacing': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+  }
+}
+
